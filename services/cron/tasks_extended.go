428009ca1fb0374fad2abeb53fe6c28604226346
Gusted <postmaster@gusted.xyz>
2022-12-18 19:56:08 +0100

    [PRIVACY] Disable `update_checker` cron tasks
    - This is being disabled as it will weekly connect to a domain.
    - This only affects existing installations, as new installations will
    have a explicit value being written into app.ini due to https://github.com/go-gitea/gitea/pull/21655

--- services/cron/tasks_extended.go
+++ services/cron/tasks_extended.go
@@ -150,7 +150,7 @@ func registerUpdateGiteaChecker() {
 	}
 	RegisterTaskFatal("update_checker", &UpdateCheckerConfig{
 		BaseConfig: BaseConfig{
-			Enabled:    true,
+			Enabled:    false,
 			RunAtStart: false,
 			Schedule:   "@every 168h",
 		},
