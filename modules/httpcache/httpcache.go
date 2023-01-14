eb9646c7ef0b1a9e73bf22806acbc2caeaee7ff3
Earl Warren <contact@earl-warren.org>
2023-01-14 08:16:30 +0100

    [BRANDING] add X-Forgejo-* headers
    (cherry picked from commit 0a3388f93f53f53cce79ebcc194d67979cbe61cc)
    (cherry picked from commit 7eba0a440a3138443d0b7461560f9f6f1c46b256)

--- modules/httpcache/httpcache.go
+++ modules/httpcache/httpcache.go
@@ -32,6 +32,7 @@ func AddCacheControlToHeader(h http.Header, maxAge time.Duration, additionalDire
 
 		// to remind users they are using non-prod setting.
 		h.Add("X-Gitea-Debug", "RUN_MODE="+setting.RunMode)
+		h.Add("X-Forgejo-Debug", "RUN_MODE="+setting.RunMode)
 	}
 
 	h.Set("Cache-Control", strings.Join(append(directives, additionalDirectives...), ", "))
