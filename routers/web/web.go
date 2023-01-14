6cd150483b06e17aee023c0afd01a3f2460b3415
Earl Warren <contact@earl-warren.org>
2023-01-14 10:07:01 +0100

    [BRANDING] [v1.20] define the forgejo webhook type
    templates/swagger/v1_json.tmpl updated with `make generate-swagger`
    
    (cherry picked from commit 88899c492efeedd138ba088a36b9c0bc733ead7b)
    (cherry picked from commit 7171bd9617c32c4911e3bdbc23c02a19e80d2465)
    (cherry picked from commit 1a742446c17aef9ca62fe75bfc0a388d40138154)
    (cherry picked from commit d7c189d7b2f9fea299a31adf068db969920ae39d)
    
    Conflicts:
    	routers/web/web.go
    (cherry picked from commit cbdea868e41fb38ca491f8b449c3e525ec82d6b9)

--- routers/web/web.go
+++ routers/web/web.go
@@ -318,6 +318,7 @@ func RegisterRoutes(m *web.Route) {
 
 	addWebhookAddRoutes := func() {
 		m.Get("/{type}/new", repo.WebhooksNew)
+		m.Post("/forgejo/new", web.Bind(forms.NewWebhookForm{}), repo.ForgejoHooksNewPost)
 		m.Post("/gitea/new", web.Bind(forms.NewWebhookForm{}), repo.GiteaHooksNewPost)
 		m.Post("/gogs/new", web.Bind(forms.NewGogshookForm{}), repo.GogsHooksNewPost)
 		m.Post("/slack/new", web.Bind(forms.NewSlackHookForm{}), repo.SlackHooksNewPost)
@@ -332,6 +333,7 @@ func RegisterRoutes(m *web.Route) {
 	}
 
 	addWebhookEditRoutes := func() {
+		m.Post("/forgejo/{id}", web.Bind(forms.NewWebhookForm{}), repo.ForgejoHooksEditPost)
 		m.Post("/gitea/{id}", web.Bind(forms.NewWebhookForm{}), repo.GiteaHooksEditPost)
 		m.Post("/gogs/{id}", web.Bind(forms.NewGogshookForm{}), repo.GogsHooksEditPost)
 		m.Post("/slack/{id}", web.Bind(forms.NewSlackHookForm{}), repo.SlackHooksEditPost)
