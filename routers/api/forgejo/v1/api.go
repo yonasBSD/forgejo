68f6bdbb3be52e9752147d75efb164ef35b4f29f
Earl Warren <contact@earl-warren.org>
2023-02-07 18:05:11 +0100

    [API] Generate the types from the OpenAPI specs

---
+++ routers/api/forgejo/v1/api.go
@@ -0,0 +1,17 @@
+// Copyright 2023 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package v1
+
+import (
+	gocontext "context"
+
+	"code.gitea.io/gitea/modules/web"
+)
+
+func Routes(ctx gocontext.Context) *web.Route {
+	m := web.NewRoute()
+	forgejo := NewForgejo()
+	m.Get("/version", forgejo.GetVersion)
+	return m
+}
