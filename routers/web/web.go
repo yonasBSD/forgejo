25161039745a63282257eded9616390241af8013
Earl Warren <contact@earl-warren.org>
2023-02-07 11:23:49 +0100

    [API] Forgejo API /api/forgejo/v1
    (cherry picked from commit 20b56692693e054bb3c04b4ef12b29b0715b4530)
    (cherry picked from commit 1574643a6a9634e5b92c033a4bfb69062a86bd05)
    
    Update semantic version according to specification
    
    (cherry picked from commit 22510f41306f9c133a7e99b61f9c38dabfd4b810)
    
    Mise à jour de 'Makefile'
    
    (cherry picked from commit c3d85d8409f1bb18a45659a167cf1ffee057f3b6)
    (cherry picked from commit 5ea23098513c068444226af41faf9be9c4c998e6)
    (cherry picked from commit ec5217b9d1b94bb04e34ce8c27eecbdc6f3a247a)
    (cherry picked from commit 14f08e364b2ad7e3176b45d3ec8da5f5605b4315)
    (cherry picked from commit b4465c67b8737aed9fd79a43046aa3795a298e68)
    
    [API] [SEMVER] replace number with version
    
    (cherry picked from commit fba48e64977a75e77c9c6cf8e3f8a679a5e7b733)
    (cherry picked from commit 532ec5d8782703b62d7d02312764e50637cd016e)
    
    [API] [SEMVER] [v1.20] less is replaced by css
    
    (cherry picked from commit 01ca3a4f4291cf4c62df42cbd12a00638f3cdc70)
    (cherry picked from commit 1d928c3ab2e1fda6082e1400d89ea96d6605ebaa)
    (cherry picked from commit a39dc804cd1da86444ed709455857f069475e7db)
    
    Conflicts:
    	webpack.config.js
    (cherry picked from commit adc68578b3eb41215338125edfca91804706f8a1)
    (cherry picked from commit 9b8d98475fd7efcc15bf0d91b0af6ef3aff64508)

--- routers/web/web.go
+++ routers/web/web.go
@@ -163,6 +163,7 @@ func Routes(ctx gocontext.Context) *web.Route {
 	if setting.API.EnableSwagger {
 		// Note: The route is here but no in API routes because it renders a web page
 		routes.Get("/api/swagger", append(common, misc.Swagger)...) // Render V1 by default
+		routes.Get("/api/forgejo/swagger", append(common, misc.SwaggerForgejo)...)
 	}
 
 	// TODO: These really seem like things that could be folded into Contexter or as helper functions
