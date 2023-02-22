0a7e43f29c5a759b041acf0300fe7ddc1feac48b
Loïc Dachary <loic@dachary.org>
2023-02-22 17:05:16 +0100

    [BRANDING] lookup .forgejo/workflows before .gitea/workflows
    (cherry picked from commit ffe51fd72f384c4644b9329aa201ea80cd7ad7ad)
    (cherry picked from commit a2b483b67d4f3c1bb214312a35abc6d81a063cde)
    (cherry picked from commit 2b7d0bbde22830d4d631421613256001b6daecfb)
    (cherry picked from commit 3027fddbff35a852f944aef5656edfbd86eb5dbf)
    (cherry picked from commit 023f7cc3b2c21993b1f43b50130a79fb32374994)
    (cherry picked from commit 95c3a3aa706045381e83c1558741f1dc02d547b2)
    (cherry picked from commit 1cc0ef5cc5e3aa74e686396f9584d16c607f37e8)
    (cherry picked from commit 467d082568b9acd668784e9705f85486c54b91c9)
    (cherry picked from commit 5d8f40e86507ae17554750498e47dad228557cb1)
    (cherry picked from commit 1d268662cc33212edba72a4e1708d4bae46e21dc)
    (cherry picked from commit 326a0fbcc1f0ca3ebd21c69c1345d19200930421)
    (cherry picked from commit 09368a49930232386e5703e3f95a84ecd939faa5)
    (cherry picked from commit 8de4830da00477369bbf982f0f2499d93019dca0)

--- modules/actions/workflows.go
+++ modules/actions/workflows.go
@@ -38,7 +38,10 @@ func IsWorkflow(path string) bool {
 }
 
 func ListWorkflows(commit *git.Commit) (git.Entries, error) {
-	tree, err := commit.SubTree(".gitea/workflows")
+	tree, err := commit.SubTree(".forgejo/workflows")
+	if _, ok := err.(git.ErrNotExist); ok {
+		tree, err = commit.SubTree(".gitea/workflows")
+	}
 	if _, ok := err.(git.ErrNotExist); ok {
 		tree, err = commit.SubTree(".github/workflows")
 	}
