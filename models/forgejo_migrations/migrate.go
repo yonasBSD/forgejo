9f0a5cab24793f0bf682ba9eed8cce6d0a36295d
oliverpool <git@olivier.pfad.fr>
2023-03-16 17:21:38 +0100

    [REFACTOR] store webhook event in database

--- models/forgejo_migrations/migrate.go
+++ models/forgejo_migrations/migrate.go
@@ -50,6 +50,8 @@ var migrations = []*Migration{
 	NewMigration("create the forgejo_repo_flag table", forgejo_v1_22.CreateRepoFlagTable),
 	// v5 -> v6
 	NewMigration("Add wiki_branch to repository", forgejo_v1_22.AddWikiBranchToRepository),
+	// v6 -> v7
+	NewMigration("Add version to hook_task", forgejo_v1_22.AddVersionToHookTaskTable),
 }
 
 // GetCurrentDBVersion returns the current Forgejo database version.
