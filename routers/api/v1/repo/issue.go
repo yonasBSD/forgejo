f061caa6555e0c9e922ee1e73dd2e4337360e9fe
fluzz <fluzz@freedroid.org>
2023-05-30 18:53:56 +0200

    Add a SetIssueUpdateDate() function in services/issue.go
    That function is used by some API calls to set the NoAutoDate and
    UpdatedUnix fields of an Issue if an updated_at date is provided.

--- routers/api/v1/repo/issue.go
+++ routers/api/v1/repo/issue.go
@@ -774,31 +774,10 @@ func EditIssue(ctx *context.APIContext) {
 		return
 	}
 
-	// In order to be set a specific update time, the DB will be updated
-	// with NoAutoTime. The 'noAutoTime' bool will be propagated down to the
-	// DB update calls to apply autoupdate or not.
-	issue.NoAutoTime = false
-	if form.Updated != nil {
-		// Check if the poster is allowed to set an update date
-		perm, err := access_model.GetUserRepoPermission(ctx, issue.Repo, ctx.Doer)
-		if err != nil {
-			ctx.Status(http.StatusForbidden)
-			return
-		}
-		if !perm.IsAdmin() && !perm.IsOwner() {
-			ctx.Error(http.StatusUnauthorized, "EditIssue", "user needs to have admin or owner right")
-			return
-		}
-
-		// A simple guard against potential inconsistent calls
-		updatedUnix := timeutil.TimeStamp(form.Updated.Unix())
-		if updatedUnix < issue.CreatedUnix || updatedUnix > timeutil.TimeStampNow() {
-			ctx.Error(http.StatusForbidden, "EditIssue", "unallowed update date")
-			return
-		}
-
-		issue.UpdatedUnix = updatedUnix
-		issue.NoAutoTime = true
+	err = issue_service.SetIssueUpdateDate(ctx, issue, form.Updated, ctx.Doer)
+	if err != nil {
+		ctx.Error(http.StatusForbidden, "SetIssueUpdateDate", err)
+		return
 	}
 
 	oldTitle := issue.Title
