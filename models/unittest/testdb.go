f0bc5f63d402e3f903e9f3d9f71c8542201f1b97
Gusted <gusted@noreply.codeberg.org>
2023-06-12 10:29:11 +0000

    [PRIVACY] Disable Gravatar by default (#837)
    - Resolves #259
    - Replaces #716 (thanks @silentcode !)
    - The main toggle _for now_ (due to the still weird settings migration to the database) is `GetDefaultDisableGravatar`, because it's used `loadPictureFrom` which in turn sets the default value for the install page which in turn is the explicit value that Forgejo will use to decide to enable or disable Gravatar after the install page.
    - Tests still rely on the fact that gravatar is enabled by default for some tests, so there's a small hack in `MainTest` for that. This is not enabled per-tests as this would otherwise just cause random test failures for when rebasing happens.
    - Unit tests ensures this value by default stays false.
    
    Co-authored-by: Gusted <postmaster@gusted.xyz>
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/837
    (cherry picked from commit c6c7c1aa1ea546571ae2f487d52c05715efe9579)

--- models/unittest/testdb.go
+++ models/unittest/testdb.go
@@ -132,6 +132,10 @@ func MainTest(m *testing.M, testOpts *TestOptions) {
 
 	setting.IncomingEmail.ReplyToAddress = "incoming+%{token}@localhost"
 
+	if err := system_model.SetSettingNoVersion(db.DefaultContext, system_model.KeyPictureDisableGravatar, "false"); err != nil {
+		fatalTestError("system_model.SetSettingNoVersion: %v\n", err)
+	}
+
 	if err = storage.Init(); err != nil {
 		fatalTestError("storage.Init: %v\n", err)
 	}
