22983c65f8b143d26600d02b2615d4b36b02000f
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2023-07-20 17:39:59 +0200

    Fix tests (tested)

--- tests/integration/user_test.go
+++ tests/integration/user_test.go
@@ -13,6 +13,7 @@ import (
 	repo_model "code.gitea.io/gitea/models/repo"
 	"code.gitea.io/gitea/models/unittest"
 	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/setting"
 	api "code.gitea.io/gitea/modules/structs"
 	"code.gitea.io/gitea/modules/test"
 	"code.gitea.io/gitea/modules/translation"
