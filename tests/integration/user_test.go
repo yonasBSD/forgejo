56a028ec96e232cfec32956f7c9908f6a059513d
Gusted <postmaster@gusted.xyz>
2023-07-20 15:54:19 +0200

    Improve testing

--- tests/integration/user_test.go
+++ tests/integration/user_test.go
@@ -4,6 +4,7 @@
 package integration
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -281,8 +282,8 @@ func TestGetOpenStreetMapLink(t *testing.T) {
 	defer tests.PrepareTestEnv(t)()
 
 	testLocations := map[string]string{
-		"Αθήνα": "%ce%91%ce%b8%ce%ae%ce%bd%ce%b1",
-		"  Αθήνα": "%20%20%ce%91%ce%b8%ce%ae%ce%bd%ce%b1",
+		"Αθήνα":                         "%ce%91%ce%b8%ce%ae%ce%bd%ce%b1",
+		"  Αθήνα":                       "%20%20%ce%91%ce%b8%ce%ae%ce%bd%ce%b1",
 		"Barbarossabrunnen, Düsseldorf": "Barbarossabrunnen%2c%20D%c3%bcsseldorf",
 		"39.91025,116.40753":            "39.91025%2c116.40753",
 		// "நியூ யோர்க்": "%e0%ae%a8%e0%ae%bf%e0%ae%af%e0%af%82%20%e0%ae%af%e0%af%8b%e0%ae%b0%e0%af%8d%e0%ae%95%e0%af%8d",
@@ -290,21 +291,21 @@ func TestGetOpenStreetMapLink(t *testing.T) {
 
 	session := loginUser(t, "user2")
 	for location, encodedLocation := range testLocations {
-		t.Logf("Testing location %s", location)
-		req := NewRequestWithValues(t, "POST", "/user/settings", map[string]string{
-			"_csrf":    GetCSRF(t, session, "/user/settings"),
-			"name":     "user2",
-			"email":    "user@example.com",
-			"language": "en-US",
-			"location": location,
-		})
-		session.MakeRequest(t, req, http.StatusSeeOther)
+		t.Run(location, func(t *testing.T) {
+			req := NewRequestWithValues(t, "POST", "/user/settings", map[string]string{
+				"_csrf":    GetCSRF(t, session, "/user/settings"),
+				"name":     "user2",
+				"email":    "user@example.com",
+				"language": "en-US",
+				"location": location,
+			})
+			session.MakeRequest(t, req, http.StatusSeeOther)
 
-		req = NewRequest(t, "GET", "/user2/")
-		resp := session.MakeRequest(t, req, http.StatusOK)
-		assert.Contains(t,
-			resp.Body.String(),
-			"<a href=\""+"https://www.openstreetmap.org/search?query="+encodedLocation+"\" rel=\"nofollow noreferrer\">",
-		)
+			req = NewRequest(t, "GET", "/user2/")
+			resp := session.MakeRequest(t, req, http.StatusOK)
+			htmlDoc := NewHTMLParser(t, resp.Body)
+
+			htmlDoc.AssertElement(t, fmt.Sprintf("a[href='https://www.openstreetmap.org/search?query=%s']", encodedLocation), true)
+		})
 	}
 }
