9568eab62af65c75f84494d4a3f65b1a74b5f1c7
erik <erik.seiert@meissa-gmbh.de>
2023-11-24 13:23:03 +0100

    Fix tests

--- models/activitypub/actor_test.go
+++ models/activitypub/actor_test.go
@@ -5,13 +5,34 @@ package activitypub
 
 import (
 	"testing"
+
+	"code.gitea.io/gitea/modules/forgefed"
+	ap "github.com/go-ap/activitypub"
 )
 
+var emptyMockStar *forgefed.Star = &forgefed.Star{
+	Source: "",
+	Activity: ap.Activity{
+		Actor:  ap.IRI(""),
+		Type:   "Star",
+		Object: ap.IRI(""),
+	},
+}
+
+var mockStar *forgefed.Star = &forgefed.Star{
+	Source: "forgejo",
+	Activity: ap.Activity{
+		Actor:  ap.IRI("https://repo.prod.meissa.de/api/v1/activitypub/user-id/1"),
+		Type:   "Star",
+		Object: ap.IRI("https://codeberg.org/api/v1/activitypub/repository-id/1"),
+	},
+}
+
 func TestActorParserEmpty(t *testing.T) {
-	item := ""
+	item := emptyMockStar
 	want := ActorID{}
 
-	got, _ := ParseActorID(item)
+	got, _ := ParseActorFromStarActivity(item)
 
 	if got != want {
 		t.Errorf("ParseActorID returned non empty actor id for empty input.")
@@ -19,26 +40,28 @@ func TestActorParserEmpty(t *testing.T) {
 }
 
 func TestActorParserValid(t *testing.T) {
-	item := "https://repo.prod.meissa.de/api/v1/activitypub/user-id/1"
+	item := mockStar
 	want := ActorID{
-		schema: "https",
 		userId: "1",
+		source: "forgejo",
+		schema: "https",
 		path:   "/api/v1/activitypub/user-id/1",
 		host:   "repo.prod.meissa.de",
 		port:   "",
 	}
 
-	got, _ := ParseActorID(item)
+	got, _ := ParseActorFromStarActivity(item)
 
 	if got != want {
-		t.Errorf("ParseActorID did not return want: %v.", want)
+		t.Errorf("\nParseActorID did not return want: %v\n but %v", want, got)
 	}
 }
 
 func TestValidateValid(t *testing.T) {
 	item := ActorID{
-		schema: "https",
 		userId: "1",
+		source: "forgejo",
+		schema: "https",
 		path:   "/api/v1/activitypub/user-id/1",
 		host:   "repo.prod.meissa.de",
 		port:   "",
@@ -50,9 +73,9 @@ func TestValidateValid(t *testing.T) {
 }
 
 func TestValidateInvalid(t *testing.T) {
-	item := "123456"
+	item := emptyMockStar
 
-	actor, _ := ParseActorID(item)
+	actor, _ := ParseActorFromStarActivity(item)
 
 	if valid, _ := actor.IsValid(); valid {
 		t.Errorf("Actor was valid with invalid input.")
