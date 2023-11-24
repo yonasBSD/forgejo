43ac3ddca9f4b7ef672837ba253c4d6b73fe34cf
erik <erik.seiert@meissa-gmbh.de>
2023-11-24 12:50:15 +0100

    Remove star validation as it happens in actor validation now

--- modules/forgefed/star.go
+++ modules/forgefed/star.go
@@ -4,8 +4,6 @@
 package forgefed
 
 import (
-	"fmt"
-
 	ap "github.com/go-ap/activitypub"
 	"github.com/valyala/fastjson"
 )
@@ -42,16 +40,6 @@ func StarNew(id ap.ID, ob ap.ID) *Star { // ToDo: May be used later in creating 
 	a := ap.ActivityNew(id, StarType, ob)
 	o := Star{Activity: *a, Source: ForgejoSourceType}
 	return &o
-}
-
-func (a Star) ValidateStar() error {
-
-	if a.Source != "forgejo" {
-		return fmt.Errorf("currently, only a forgejo source is supported")
-	}
-
-	return nil
-
 }
 
 func (a Star) MarshalJSON() ([]byte, error) {
