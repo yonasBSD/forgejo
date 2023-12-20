4b744399229f255eb124c22e3969715046043209
Gergely Nagy <forgejo@gergo.csillger.hu>
2023-12-20 21:44:55 +0100

    Optionally allow anyone to edit Wikis
    This is largely based on gitea#6312 by @ashimokawa, with updates and
    fixes by myself, and incorporates the review feedback given in that pull
    request, and more.
    
    What this patch does is add a new "default_permissions" column to the
    `repo_units` table (defaulting to read permission), adjusts the
    permission checking code to take this into consideration, and then
    exposes a setting that lets a repo administrator enable any user on a
    Forgejo instance to edit the repo's wiki (effectively giving the wiki
    unit of the repo "write" permissions by default).
    
    By default, wikis will remain restricted to collaborators, but with the
    new setting exposed, they can be turned into globally editable wikis.
    
    Fixes Codeberg/Community#28.
    
    Signed-off-by: Gergely Nagy <forgejo@gergo.csillger.hu>

--- services/forms/repo_form.go
+++ services/forms/repo_form.go
@@ -140,6 +140,7 @@ type RepoSettingForm struct {
 	// Advanced settings
 	EnableCode                            bool
 	EnableWiki                            bool
+	GloballyWriteableWiki                 bool
 	EnableExternalWiki                    bool
 	ExternalWikiURL                       string
 	EnableIssues                          bool
