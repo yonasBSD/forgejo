70e55b62667763036a6f105f038462ed88a3347e
Gergely Nagy <forgejo@gergo.csillger.hu>
2023-12-29 15:50:47 +0100

    settings: charset & collate settings for the db
    Remove the (undocumented) `Database.MysqlCharset` setting, and
    introduce `Database.DefaultCharset` and `Database.DefaultCollation`
    settings instead.
    
    The reason for the `MysqlCharset` removal is that in a later patch,
    we're going to adjust the engine initialization so that it achieves
    similar results, in a different way.
    
    The new settings will remain undocumented for similar reasons
    `MysqlCharset` was undocumented: the defaults work for the vast majority
    of cases, and it's too easy to break things if changing them.
    
    Signed-off-by: Gergely Nagy <forgejo@gergo.csillger.hu>

--- modules/setting/database.go
+++ modules/setting/database.go
@@ -34,7 +34,6 @@ var (
 		SSLMode           string
 		Path              string
 		LogSQL            bool
-		MysqlCharset      string
 		Timeout           int // seconds
 		SQLiteJournalMode string
 		DBConnectRetries  int
@@ -45,6 +44,8 @@ var (
 		IterateBufferSize int
 		AutoMigration     bool
 		SlowQueryTreshold time.Duration
+		DefaultCharset    string
+		DefaultCollation  string
 	}{
 		Timeout:           500,
 		IterateBufferSize: 50,
@@ -68,7 +69,6 @@ func loadDBSetting(rootCfg ConfigProvider) {
 	}
 	Database.Schema = sec.Key("SCHEMA").String()
 	Database.SSLMode = sec.Key("SSL_MODE").MustString("disable")
-	Database.MysqlCharset = sec.Key("MYSQL_CHARSET").MustString("utf8mb4") // do not document it, end users won't need it.
 
 	Database.Path = sec.Key("PATH").MustString(filepath.Join(AppDataPath, "gitea.db"))
 	Database.Timeout = sec.Key("SQLITE_TIMEOUT").MustInt(500)
@@ -88,6 +88,9 @@ func loadDBSetting(rootCfg ConfigProvider) {
 	Database.DBConnectBackoff = sec.Key("DB_RETRY_BACKOFF").MustDuration(3 * time.Second)
 	Database.AutoMigration = sec.Key("AUTO_MIGRATION").MustBool(true)
 	Database.SlowQueryTreshold = sec.Key("SLOW_QUERY_TRESHOLD").MustDuration(5 * time.Second)
+
+	Database.DefaultCharset = sec.Key("DEFAULT_CHARSET").String()
+	Database.DefaultCollation = sec.Key("DEFAULT_COLLATION").String()
 }
 
 // DBConnStr returns database connection string
@@ -107,8 +110,8 @@ func DBConnStr() (string, error) {
 		if tls == "disable" { // allow (Postgres-inspired) default value to work in MySQL
 			tls = "false"
 		}
-		connStr = fmt.Sprintf("%s:%s@%s(%s)/%s%scharset=%s&parseTime=true&tls=%s",
-			Database.User, Database.Passwd, connType, Database.Host, Database.Name, paramSep, Database.MysqlCharset, tls)
+		connStr = fmt.Sprintf("%s:%s@%s(%s)/%s%sparseTime=true&tls=%s",
+			Database.User, Database.Passwd, connType, Database.Host, Database.Name, paramSep, tls)
 	case "postgres":
 		connStr = getPostgreSQLConnectionString(Database.Host, Database.User, Database.Passwd, Database.Name, Database.SSLMode)
 	case "mssql":
