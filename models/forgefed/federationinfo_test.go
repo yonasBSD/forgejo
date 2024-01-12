bbccc24ed1ea2c3e5f6bea1970649ac8df45daf6
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-01-12 15:34:34 +0100

    test federationinfo validation

---
+++ models/forgefed/federationinfo_test.go
@@ -0,0 +1,24 @@
+// Copyright 2024 The forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package forgefed
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/modules/timeutil"
+	"code.gitea.io/gitea/modules/validation"
+)
+
+func Test_ValidateMaxLen(t *testing.T) {
+	sut := FederationInfo{
+		HostFqdn: "host.do.main",
+		NodeInfo: NodeInfo{
+			Source: "forgejo",
+		},
+		LatestActivity: timeutil.TimeStampNow(),
+	}
+	if res, err := validation.IsValid(sut); !res {
+		t.Errorf("sut should be valid but was %q", err)
+	}
+}
