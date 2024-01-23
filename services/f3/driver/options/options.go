83b5bc3d50cac132a5916d4b1cf72f0f9be1d605
Earl Warren <contact@earl-warren.org>
2024-01-23 12:45:02 +0000

    [F3] driver stub
    (cherry picked from commit ed609d02482c025bc4238994455cdeadaa838d34)
    (cherry picked from commit 738e711770763fdfcad38d3375ceaeef2149c553)

---
+++ services/f3/driver/options/options.go
@@ -0,0 +1,29 @@
+// Copyright Earl Warren <contact@earl-warren.org>
+// Copyright Loïc Dachary <loic@dachary.org>
+// SPDX-License-Identifier: MIT
+
+package options
+
+import (
+	"net/http"
+
+	"lab.forgefriends.org/friendlyforgeformat/gof3/options"
+	"lab.forgefriends.org/friendlyforgeformat/gof3/options/logger"
+)
+
+type NewMigrationHTTPClientFun func() *http.Client
+
+type Options struct {
+	options.Options
+	logger.OptionsLogger
+
+	NewMigrationHTTPClient NewMigrationHTTPClientFun
+}
+
+func (o *Options) GetNewMigrationHTTPClient() NewMigrationHTTPClientFun {
+	return o.NewMigrationHTTPClient
+}
+
+func (o *Options) SetNewMigrationHTTPClient(fun NewMigrationHTTPClientFun) {
+	o.NewMigrationHTTPClient = fun
+}
