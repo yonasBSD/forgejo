df2737ec0d4675cac9a02c66bab5d5a66e4e0396
Earl Warren <contact@earl-warren.org>
2024-01-23 09:43:29 +0000

    [F3] setting [F3].ENABLED
    (cherry picked from commit b11c68b73413a86ee171589685fe8708fb52179e)
    (cherry picked from commit e6ebb381a27564a2436a6d22a49167fdb2e46972)

--- modules/setting/setting.go
+++ modules/setting/setting.go
@@ -221,6 +221,7 @@ func LoadSettings() {
 	loadProjectFrom(CfgProvider)
 	loadMimeTypeMapFrom(CfgProvider)
 	loadFederationFrom(CfgProvider)
+	loadF3From(CfgProvider)
 }
 
 // LoadSettingsForInstall initializes the settings for install
