85516f58d1fa136a3cb35b0fc31af9d11c7a49f9
Earl Warren <contact@earl-warren.org>
2024-02-03 12:18:06 +0100

    [F3] driver/users implementation
    (cherry picked from commit 53d3d74c8bac8a6d614a5adfd5e4f436f76296e9)

---
+++ services/f3/driver/user.go
@@ -0,0 +1,17 @@
+// Copyright Earl Warren <contact@earl-warren.org>
+// Copyright Loïc Dachary <loic@dachary.org>
+// SPDX-License-Identifier: MIT
+
+package driver
+
+import (
+	"lab.forgefriends.org/friendlyforgeformat/gof3/tree/generic"
+)
+
+type user struct {
+	common
+}
+
+func newUser() generic.NodeDriverInterface {
+	return &user{}
+}
--- services/f3/driver/users.go
+++ services/f3/driver/users.go
@@ -5,11 +5,42 @@
 package driver
 
 import (
+	"context"
+	"fmt"
+
+	"code.gitea.io/gitea/models/db"
+	user_model "code.gitea.io/gitea/models/user"
+
+	f3_tree "lab.forgefriends.org/friendlyforgeformat/gof3/tree/f3"
 	"lab.forgefriends.org/friendlyforgeformat/gof3/tree/generic"
 )
 
 type users struct {
 	container
+}
+
+func (o *users) ListPage(ctx context.Context, page int) generic.ChildrenSlice {
+	sess := db.GetEngine(ctx).In("type", user_model.UserTypeIndividual, user_model.UserTypeF3)
+	if page != 0 {
+		sess = db.SetSessionPagination(sess, &db.ListOptions{Page: page, PageSize: o.getPageSize()})
+	}
+	sess = sess.Select("`user`.*")
+	users := make([]*user_model.User, 0, o.getPageSize())
+
+	if err := sess.Find(&users); err != nil {
+		panic(fmt.Errorf("error while listing users: %v", err))
+	}
+
+	return f3_tree.ConvertListed(ctx, o.GetNode(), f3_tree.ConvertToAny(users...)...)
+}
+
+func (o *users) GetIDFromName(ctx context.Context, name string) generic.NodeID {
+	user, err := user_model.GetUserByName(ctx, name)
+	if err != nil {
+		panic(fmt.Errorf("GetUserByName: %v", err))
+	}
+
+	return generic.NodeID(fmt.Sprintf("%d", user.ID))
 }
 
 func newUsers() generic.NodeDriverInterface {
