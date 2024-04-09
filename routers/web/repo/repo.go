2b9977d843bc5717bafac891977118225a699961
erik <erik.seiert@meissa-gmbh.de>
2024-04-09 11:46:11 +0200

    Linting

--- routers/web/repo/repo.go
+++ routers/web/repo/repo.go
@@ -333,7 +333,6 @@ func ActionWatch(watch bool) func(ctx *context.Context) {
 func ActionStar(star bool) func(ctx *context.Context) {
 	return func(ctx *context.Context) {
 		err := repo_service.StarRepoAndFederate(ctx, *ctx.Doer, ctx.Repo.Repository.ID, star)
-
 		if err != nil {
 			ctx.ServerError(fmt.Sprintf("Action (star, %t)", star), err)
 			return
