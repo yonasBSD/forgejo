5a2b2fcec24ab157441844539ed1b3856169a63e
Mai-Lapyst <mai-lapyst@noreply.codeberg.org>
2024-05-23 18:06:16 +0200

    Move dispatching of workflows into the actions service

--- routers/web/repo/actions/manual.go
+++ routers/web/repo/actions/manual.go
@@ -1,24 +1,13 @@
+// Copyright The Forgejo Authors.
+// SPDX-License-Identifier: MIT
+
 package actions
 
 import (
-	"bytes"
 	"net/url"
-	"strconv"
 
-	actions_model "code.gitea.io/gitea/models/actions"
-	"code.gitea.io/gitea/models/perm"
-	"code.gitea.io/gitea/models/perm/access"
-	"code.gitea.io/gitea/modules/actions"
-	"code.gitea.io/gitea/modules/git"
-	"code.gitea.io/gitea/modules/json"
-	"code.gitea.io/gitea/modules/setting"
-	"code.gitea.io/gitea/modules/structs"
-	"code.gitea.io/gitea/modules/webhook"
+	actions_service "code.gitea.io/gitea/services/actions"
 	context_module "code.gitea.io/gitea/services/context"
-	"code.gitea.io/gitea/services/convert"
-
-	"github.com/nektos/act/pkg/jobparser"
-	act_model "github.com/nektos/act/pkg/model"
 )
 
 func ManualRunWorkflow(ctx *context_module.Context) {
@@ -42,131 +31,28 @@ func ManualRunWorkflow(ctx *context_module.Context) {
 		return
 	}
 
-	commit, err := ctx.Repo.GitRepo.GetCommit(ref)
+	workflow, err := actions_service.GetWorkflowFromCommit(ctx.Repo, ref, workflowID)
 	if err != nil {
-		ctx.ServerError("GetCommit", err)
+		ctx.ServerError("GetWorkflowFromCommit", err)
 		return
-	}
-
-	entries, err := actions.ListWorkflows(commit)
-	if err != nil {
-		ctx.ServerError("ListWorkflows", err)
-		return
-	}
-
-	var workflowEntry *git.TreeEntry
-	for _, entry := range entries {
-		if entry.Name() == workflowID {
-			workflowEntry = entry
-			break
-		}
-	}
-	if workflowEntry == nil {
-		ctx.NotFound("workflow in ListWorkflows", nil)
-		return
-	}
-
-	content, err := actions.GetContentFromEntry(workflowEntry)
-	if err != nil {
-		ctx.ServerError("GetContentFromEntry", err)
-		return
-	}
-	wf, err := act_model.ReadWorkflow(bytes.NewReader(content))
-	if err != nil {
-		ctx.ServerError("ReadWorkflow", err)
-		return
-	}
-
-	fullWorkflowID := ".forgejo/workflows/" + workflowID
-
-	title := wf.Name
-	if len(title) < 1 {
-		title = fullWorkflowID
 	}
 
 	location := ctx.Repo.RepoLink + "/actions?workflow=" + url.QueryEscape(workflowID) +
 		"&actor=" + url.QueryEscape(ctx.FormString("actor")) +
 		"&status=" + url.QueryEscape(ctx.FormString("status"))
 
-	inputs := make(map[string]string)
-	if workflowDispatch := wf.WorkflowDispatchConfig(); workflowDispatch != nil {
-		for key, input := range workflowDispatch.Inputs {
-			formKey := "inputs[" + key + "]"
-			val := ctx.FormString(formKey)
-			if len(val) == 0 {
-				val = input.Default
-				if len(val) == 0 {
-					if input.Required {
-						name := input.Description
-						if len(name) == 0 {
-							name = key
-						}
-						ctx.Flash.Error(ctx.Locale.Tr("actions.workflow.dispatch.input_required", name))
-						ctx.Redirect(location)
-						return
-					}
-					continue
-				}
-			} else {
-				switch input.Type {
-				case "boolean":
-					// Since "boolean" inputs are rendered as a checkbox in html, the value inside the form is "on"
-					val = strconv.FormatBool(val == "on")
-				}
-			}
-			inputs[key] = val
+	formKeyGetter := func(key string) string {
+		formKey := "inputs[" + key + "]"
+		return ctx.FormString(formKey)
+	}
+
+	if err := workflow.Dispatch(ctx, formKeyGetter, ctx.Repo, ctx.Doer); err != nil {
+		if actions_service.IsInputRequiredErr(err) {
+			ctx.Flash.Error(ctx.Locale.Tr("actions.workflow.dispatch.input_required", err.(actions_service.InputRequiredErr).Name))
+			ctx.Redirect(location)
+			return
 		}
-	}
-
-	if int64(len(inputs)) > setting.Actions.LimitDispatchInputs {
-		ctx.ServerError("ToManyInputs", nil)
-		return
-	}
-
-	payload := &structs.WorkflowDispatchPayload{
-		Inputs:     inputs,
-		Ref:        ref,
-		Repository: convert.ToRepo(ctx, ctx.Repo.Repository, access.Permission{AccessMode: perm.AccessModeNone}),
-		Sender:     convert.ToUser(ctx, ctx.Doer, nil),
-		Workflow:   fullWorkflowID,
-	}
-
-	p, err := json.Marshal(payload)
-	if err != nil {
-		ctx.ServerError("json.Marshal", err)
-		return
-	}
-
-	run := &actions_model.ActionRun{
-		Title:         title,
-		RepoID:        ctx.Repo.Repository.ID,
-		Repo:          ctx.Repo.Repository,
-		OwnerID:       ctx.Repo.Repository.OwnerID,
-		WorkflowID:    workflowID,
-		TriggerUserID: ctx.Doer.ID,
-		TriggerUser:   ctx.Doer,
-		Ref:           ref,
-		CommitSHA:     commit.ID.String(),
-		Event:         webhook.HookEventWorkflowDispatch,
-		EventPayload:  string(p),
-		TriggerEvent:  string(webhook.HookEventWorkflowDispatch),
-		Status:        actions_model.StatusWaiting,
-	}
-
-	vars, err := actions_model.GetVariablesOfRun(ctx, run)
-	if err != nil {
-		ctx.ServerError("GetVariablesOfRun", err)
-		return
-	}
-
-	jobs, err := jobparser.Parse(content, jobparser.WithVars(vars))
-	if err != nil {
-		ctx.ServerError("jobparser.Parse", err)
-		return
-	}
-
-	if err := actions_model.InsertRun(ctx, run, jobs); err != nil {
-		ctx.ServerError("InsertRun", err)
+		ctx.ServerError("workflow.Dispatch", err)
 		return
 	}
 
