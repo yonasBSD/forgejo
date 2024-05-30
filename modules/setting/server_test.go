2b96088e287e7c295b439f7904e033c3fe7079f7
mirko <mirko.perillo@gmail.com>
2024-05-30 14:46:52 +0200

    Add unit tests for display name generation

---
+++ modules/setting/server_test.go
@@ -0,0 +1,33 @@
+package setting
+
+import (
+	"testing"
+
+	"code.gitea.io/gitea/modules/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDisplayNameDefault(t *testing.T) {
+	defer test.MockVariableValue(&AppName, "Forgejo")()
+	defer test.MockVariableValue(&AppSlogan, "Beyond coding. We Forge.")()
+	defer test.MockVariableValue(&AppDisplayNameFormat, "{APP_NAME}: {APP_SLOGAN}")()
+	displayName := generateDisplayName()
+	assert.Equal(t, "Forgejo: Beyond coding. We Forge.", displayName)
+}
+
+func TestDisplayNameUnsetSlogan(t *testing.T) {
+	defer test.MockVariableValue(&AppName, "Forgejo")()
+	defer test.MockVariableValue(&AppSlogan, "unset")()
+	defer test.MockVariableValue(&AppDisplayNameFormat, "{APP_NAME}: {APP_SLOGAN}")()
+	displayName := generateDisplayName()
+	assert.Equal(t, "Forgejo", displayName)
+}
+
+func TestDisplayNameCustomFormat(t *testing.T) {
+	defer test.MockVariableValue(&AppName, "Forgejo")()
+	defer test.MockVariableValue(&AppSlogan, "Beyond coding. We Forge.")()
+	defer test.MockVariableValue(&AppDisplayNameFormat, "{APP_NAME} - {APP_SLOGAN}")()
+	displayName := generateDisplayName()
+	assert.Equal(t, "Forgejo - Beyond coding. We Forge.", displayName)
+}
+
