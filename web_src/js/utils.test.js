989c29d048e6bef17bb346c4b4e2eb757a66ff9a
mritunjayr <mritunjaykumar694@gmail.com>
2024-06-25 21:41:29 +0530

    utils.test.js added more unit test

--- web_src/js/utils.test.js
+++ web_src/js/utils.test.js
@@ -1,7 +1,7 @@
 import {
   basename, extname, isObject, stripTags, parseIssueHref,
   parseUrl, translateMonth, translateDay, blobToDataURI,
-  toAbsoluteUrl, encodeURLEncodedBase64, decodeURLEncodedBase64, isDarkTheme, sleep,
+  toAbsoluteUrl, encodeURLEncodedBase64, decodeURLEncodedBase64, isDarkTheme, sleep, convertImage,
 } from './utils.js';
 
 test('basename', () => {
@@ -131,4 +131,68 @@ test('should return true if dark theme is enabled', () => {
 test('should return false if dark theme is disabled', () => {
   document.documentElement.style.setProperty('--is-dark-theme', 'false');
   expect(isDarkTheme()).toBe(false);
+});
+
+const data = new Uint8Array([
+  137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 8,
+  0, 0, 0, 8, 8, 2, 0, 0, 0, 75, 109, 41, 220, 0, 0, 0, 34, 73, 68, 65, 84,
+  8, 215, 99, 120, 173, 168, 135, 21, 49, 0, 241, 255, 15, 90, 104, 8, 33,
+  129, 83, 7, 97, 163, 136, 214, 129, 93, 2, 43, 2, 0, 181, 31, 90, 179,
+  225, 252, 176, 37, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130,
+]);
+const blob = new Blob([data], {type: 'image/png'});
+test('convertImage', async () => {
+  HTMLCanvasElement.prototype.getContext = vi.fn(() => {
+    return {
+      drawImage: vi.fn(),
+    };
+  });
+  HTMLCanvasElement.prototype.toBlob = vi.fn((callback) => {
+    callback(blob);
+  });
+  HTMLImageElement.prototype.addEventListener = vi.fn((_, callback) => {
+    callback();
+  });
+  const response = await convertImage(blob, 'image/png');
+  expect(response).toBe(blob);
+  HTMLCanvasElement.prototype.toBlob = vi.fn((callback) => {
+    callback();
+  });
+  try {
+    await convertImage(blob, 'image/png');
+  } catch (err) {
+    expect(err).toBeInstanceOf(Error);
+  }
+
+  HTMLCanvasElement.prototype.toBlob = vi.fn(() => {
+    throw new Error('Custom Inside Error');
+  });
+  try {
+    await convertImage(blob, 'image/png');
+  } catch (err) {
+    expect(err).toBeInstanceOf(Error);
+  }
+  try {
+    vi.spyOn(window, 'Image').mockImplementationOnce = vi.fn(() => {
+      throw new Error('Custom Outside Error');
+    });
+    await convertImage(blob, 'image/png');
+  } catch (err) {
+    expect(err).toBeInstanceOf(Error);
+  }
+});
+
+test('should expect error from blobToDataURI', async () => {
+  try {
+    vi.spyOn(window, 'FileReader').mockImplementationOnce(() => {
+      this.addEventListener = vi.fn((_, callback) => callback());
+      this.readAsDataURL = vi.fn(() => {
+        this.result = 'data:text/plain;base64,invalid';
+        this.dispatchEvent(new Event('error'));
+      });
+    });
+    await blobToDataURI(blob);
+  } catch (err) {
+    expect(err).toBeInstanceOf(Error);
+  }
 });
