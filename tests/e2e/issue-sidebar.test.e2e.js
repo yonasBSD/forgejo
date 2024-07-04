3120cdd544e5ad3b360f94643c37f5aa4b50918a
Otto Richter <git@otto.splvs.net>
2024-07-04 20:09:56 +0200

    I always forget about linters - I was happy after the tests passed

--- tests/e2e/issue-sidebar.test.e2e.js
+++ tests/e2e/issue-sidebar.test.e2e.js
@@ -11,26 +11,30 @@ async function login({browser}, workerInfo) {
   return await context.newPage();
 }
 
+// belongs to test: Pull: Toggle WIP
+const prTitle = 'pull5';
+
+async function click_toggle_wip({page}) {
+  await page.locator('.toggle-wip>a').click();
+  await page.waitForLoadState('networkidle');
+}
+
+async function check_wip({page}, is) {
+  const elemTitle = '#issue-title-display';
+  const stateLabel = '.issue-state-label';
+  await expect(page.locator(elemTitle)).toContainText(prTitle);
+  await expect(page.locator(elemTitle)).toContainText('#5');
+  if (is) {
+    await expect(page.locator(elemTitle)).toContainText('WIP');
+    await expect(page.locator(stateLabel)).toContainText('Draft');
+  } else {
+    await expect(page.locator(elemTitle)).not.toContainText('WIP');
+    await expect(page.locator(stateLabel)).toContainText('Open');
+  }
+}
+
 test('Pull: Toggle WIP', async ({browser}, workerInfo) => {
   test.skip(workerInfo.project.name === 'Mobile Safari', 'Unable to get tests working on Safari Mobile, see https://codeberg.org/forgejo/forgejo/pulls/3445#issuecomment-1789636');
-  async function click_toggle_wip({page}) {
-    await page.locator('.toggle-wip>a').click();
-    await page.waitForLoadState('networkidle');
-  }
-  async function check_wip({page}, is) {
-    const elemTitle = '#issue-title-display';
-    const stateLabel = '.issue-state-label';
-    await expect(page.locator(elemTitle)).toContainText(prTitle);
-    await expect(page.locator(elemTitle)).toContainText('#5');
-    if (is) {
-      await expect(page.locator(elemTitle)).toContainText('WIP');
-      await expect(page.locator(stateLabel)).toContainText('Draft');
-    } else {
-      await expect(page.locator(elemTitle)).not.toContainText('WIP');
-      await expect(page.locator(stateLabel)).toContainText('Open');
-    }
-  }
-  const prTitle = 'pull5';
   const page = await login({browser}, workerInfo);
   const response = await page.goto('/user2/repo1/pulls/5');
   await expect(response?.status()).toBe(200); // Status OK
