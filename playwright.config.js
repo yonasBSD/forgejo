2fd38d6cd0d5f5501dc3a77751936805c38f33a7
Earl Warren <contact@earl-warren.org>
2024-07-08 01:05:14 +0200

    tests: temporarily reduce e2e to using chromium
    * there are too many false negative
    * it takes too long to run and should be in a separate workflow
    * run when the web_src/js/** files are modified otherwise it is
      kind of pointless

--- playwright.config.js
+++ playwright.config.js
@@ -71,33 +71,6 @@ export default {
       },
     },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-
-    /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
-    },
-    {
-      name: 'Mobile Safari',
-      use: {
-        ...devices['iPhone 12'],
-      },
-    },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
