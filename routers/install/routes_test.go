bfb71325fac5a0a914c22b9a8ec5b0e5607d0926
TheFox0x7 <thefox0x7@gmail.com>
2024-07-08 17:23:17 +0200

    test(opentelemetry): add coverage for otelchi

--- routers/install/routes_test.go
+++ routers/install/routes_test.go
@@ -4,10 +4,21 @@
 package install
 
 import (
+	"context"
+	"io"
+	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"code.gitea.io/gitea/models/unittest"
+	"code.gitea.io/gitea/modules/graceful"
+	"code.gitea.io/gitea/modules/opentelemetry"
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/modules/test"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -35,4 +46,47 @@ func TestRoutes(t *testing.T) {
 
 func TestMain(m *testing.M) {
 	unittest.MainTest(m)
+}
+
+func TestOtelChi(t *testing.T) {
+	const ServiceName = "forgejo-otelchi"
+	otelURL, ok := os.LookupEnv("TEST_OTEL_URL")
+	if !ok {
+		t.Skip("TEST_OTEL_URL not set")
+	}
+	traceEndpoint, err := url.Parse(otelURL)
+	assert.NoError(t, err)
+
+	defer test.MockVariableValue(&setting.OpenTelemetry.Resource.ServiceName, ServiceName)()
+	defer test.MockVariableValue(&setting.OpenTelemetry.Traces.Endpoint, traceEndpoint)()
+	defer test.MockVariableValue(&setting.OpenTelemetry.Traces.Insecure, true)()
+
+	assert.NoError(t, opentelemetry.Init(context.Background()))
+	r := Routes()
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/e/img/gitea.svg", nil)
+	r.ServeHTTP(w, req)
+
+	traceEndpoint.Host = traceEndpoint.Hostname() + ":16686"
+	traceEndpoint.Path = "/api/services"
+
+	namePresent := false
+
+	for i := range []int{1, 1, 2, 3, 5, 8} {
+		resp, err := http.Get(traceEndpoint.String())
+		assert.NoError(t, err)
+
+		apiResponse, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		if strings.Contains(string(apiResponse), ServiceName) {
+			namePresent = true
+			break
+		}
+		time.Sleep(time.Duration(i) * time.Second)
+	}
+	assert.True(t, namePresent)
+
+	graceful.GetManager().DoGracefulShutdown()
 }
