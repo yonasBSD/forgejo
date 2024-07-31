a0a0b17bc0d37e358c4b9b6990417cc651aa188d
TheFox0x7 <thefox0x7@gmail.com>
2024-07-31 17:51:29 +0200

    test(actions): add test for job status aggregation

---
+++ models/actions/run_test.go
@@ -0,0 +1,56 @@
+package actions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJobStatusAggregation(t *testing.T) {
+	testCases := []struct {
+		desc           string
+		runStatuses    []Status
+		expectedStatus Status
+	}{
+		{
+			desc:           "All job skipped - aggregate skip",
+			runStatuses:    []Status{StatusSkipped},
+			expectedStatus: StatusSuccess, // Should be Skipped
+		},
+		{
+			desc:           "Job success, no fails - aggregate success",
+			runStatuses:    []Status{StatusSuccess, StatusSkipped},
+			expectedStatus: StatusSuccess,
+		},
+		{
+			desc:           "Any job failure - aggregate failure",
+			runStatuses:    []Status{StatusSuccess, StatusFailure, StatusSkipped},
+			expectedStatus: StatusFailure,
+		},
+		{
+			desc:           "Any job cancelled - aggregate cancelled",
+			runStatuses:    []Status{StatusSuccess, StatusFailure, StatusCancelled, StatusSkipped},
+			expectedStatus: StatusFailure, // Should be Cancelled
+		},
+		{
+			desc:           "Any job running - aggregate running",
+			runStatuses:    []Status{StatusSuccess, StatusFailure, StatusCancelled, StatusSkipped, StatusWaiting, StatusRunning},
+			expectedStatus: StatusRunning,
+		},
+		{
+			desc:           "No jobs running, jobs waiting  - aggregate waiting",
+			runStatuses:    []Status{StatusSuccess, StatusFailure, StatusCancelled, StatusSkipped, StatusWaiting},
+			expectedStatus: StatusWaiting,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			mockedJobs := []*ActionRunJob{}
+			for _, v := range tC.runStatuses {
+				mockedJobs = append(mockedJobs, &ActionRunJob{Status: v})
+			}
+
+			assert.Equal(t, tC.expectedStatus, aggregateJobStatus(mockedJobs))
+		})
+	}
+}
