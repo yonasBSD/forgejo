73352f2e1349021e3da702d50229c387d6ad7ac6
年幼涩批 <explodingfkl@gmail.com>
2024-08-01 22:18:47 +0800

    Support for signing uploaded RPM

--- modules/setting/packages.go
+++ modules/setting/packages.go
@@ -44,6 +44,7 @@ var (
 		LimitSizeRubyGems    int64
 		LimitSizeSwift       int64
 		LimitSizeVagrant     int64
+		RPMSignEnabled       bool
 	}{
 		Enabled:              true,
 		LimitTotalOwnerCount: -1,
@@ -102,6 +103,7 @@ func loadPackagesFrom(rootCfg ConfigProvider) (err error) {
 	Packages.LimitSizeRubyGems = mustBytes(sec, "LIMIT_SIZE_RUBYGEMS")
 	Packages.LimitSizeSwift = mustBytes(sec, "LIMIT_SIZE_SWIFT")
 	Packages.LimitSizeVagrant = mustBytes(sec, "LIMIT_SIZE_VAGRANT")
+	Packages.RPMSignEnabled = sec.Key("RPM_SIGN_ENABLED").MustBool(false)
 	return nil
 }
 
