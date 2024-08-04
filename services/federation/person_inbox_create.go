8717d773534e525f951447a17c345cc699b948cd
Gergely Nagy <forgejo@gergo.csillger.hu>
2024-08-04 23:56:15 +0200

    [skip ci] services/federation: logging
    Signed-off-by: Gergely Nagy <forgejo@gergo.csillger.hu>

--- services/federation/person_inbox_create.go
+++ services/federation/person_inbox_create.go
@@ -9,6 +9,7 @@ import (
 
 	"code.gitea.io/gitea/models/forgefed"
 	fm "code.gitea.io/gitea/modules/forgefed"
+	"code.gitea.io/gitea/modules/log"
 	"code.gitea.io/gitea/modules/validation"
 	context_service "code.gitea.io/gitea/services/context"
 
@@ -19,11 +20,13 @@ func processPersonInboxCreate(ctx *context_service.APIContext, activity *ap.Acti
 	createAct := fm.ForgeUserActivity{*activity}
 
 	if res, err := validation.IsValid(createAct); !res {
+		log.Error("Invalid user activity: %v", activity)
 		ctx.Error(http.StatusNotAcceptable, "Invalid user activity", err)
 		return
 	}
 
 	if createAct.Object.GetType() != ap.NoteType {
+		log.Error("Invalid object type for Create activity: %v", createAct.Object.GetType())
 		ctx.Error(http.StatusNotAcceptable, "Invalid object type for Create activity", fmt.Errorf("Invalid object type for Create activity: %v", createAct.Object.GetType()))
 		return
 	}
@@ -34,10 +37,12 @@ func processPersonInboxCreate(ctx *context_service.APIContext, activity *ap.Acti
 
 	actorURI := act.Actor.GetLink().String()
 	if _, _, _, err := findOrCreateFederatedUser(ctx, actorURI); err != nil {
+		log.Error("Error finding or creating federated user (%s): %v", actorURI, err)
 		return
 	}
 
 	if err := forgefed.AddUserActivity(ctx, ctx.ContextUser.ID, actorURI, &userActivity); err != nil {
+		log.Error("Unable to record activity: %v", err)
 		ctx.Error(http.StatusInternalServerError, "Unable to record activity", err)
 		return
 	}
