225c78a1d6df7aec4e1469bfbb874b83abcbf03e
Radosław Piliszek <radek@piliszek.it>
2024-08-27 17:11:48 +0200

    ui(code-search): fix to use CodeSearchMode
    Drop unused IsFuzzy and IsRegExp

--- routers/web/explore/code.go
+++ routers/web/explore/code.go
@@ -45,7 +45,6 @@ func Code(ctx *context.Context) {
 	ctx.Data["Keyword"] = keyword
 	ctx.Data["Language"] = language
 	ctx.Data["CodeSearchOptions"] = []string{"exact", "fuzzy"}
-	ctx.Data["IsFuzzy"] = isFuzzy
 	if isFuzzy {
 		ctx.Data["CodeSearchMode"] = "fuzzy"
 	} else {
