dc9a268d3c23d19e42fabd936c9846828f94a6e4
Otto Richter <git@otto.splvs.net>
2024-09-22 21:21:54 +0200

    i18n: UX improvements: Team permissions and issue closing
    Change word order for issue comment actions
    -  An attempt to address https://codeberg.org/forgejo/forgejo/issues/2650
    
    Org team permissions improvements
    
    - consistency: added missing dot
    - clarity: explain what external units mean
    - use dedicated keys to explain the permissions.
    - split in read/write permissions
    - use explicit labels for accessibility
    - ext_wiki.desc and ext_issues.desc are no longer in use.

--- tests/e2e/shared/forms.js
+++ tests/e2e/shared/forms.js
@@ -21,10 +21,24 @@ export async function validate_form({page}, scope) {
     await expect(b).toHaveCSS('margin-top', '0px');
     await expect(b).toHaveCSS('vertical-align', 'baseline');
   }
+
   // assert no (trailing) colon is used in labels
   // might be necessary to adjust in case colons are strictly necessary in help text
   for (const l of await page.locator('label').all()) {
     const str = await l.textContent();
     await expect(str.split('\n')[0]).not.toContain(':');
+  }
+
+  // check that multiple help text are correctly aligned to each other
+  // used for example to separate read/write permissions in team permission matrix
+  for (const l of await page.locator('label:has(.help + .help)').all()) {
+    const helpLabels = await l.locator('.help').all();
+    const boxes = await Promise.all(helpLabels.map((help) => help.boundingBox()));
+    for (let i = 1; i < boxes.length; i++) {
+      // help texts vertically aligned on top of each other
+      await expect(boxes[i].x).toBe(boxes[0].x);
+      // help texts don't horizontally intersect each other
+      await expect(boxes[i].y + boxes[i].height).toBeGreaterThanOrEqual(boxes[i - 1].y + boxes[i - 1].height);
+    }
   }
 }
