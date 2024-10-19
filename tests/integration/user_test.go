Apply-Creatures/creature-foja

commit 12e23ee19916f4ca3f10916be8bb8666fd0d6f58

[Refactor] Unify repo search order by logic (#30876)
have repo OrderBy definitions defined in one place and use a single type
for OrderBy database options

(cherry picked from commit bb04311b0b5b7a28f94c4bc409db1c4a04bcef17)

models/repo/search.go
@@ -5,20 +5,72 @@ package repo
 
 import "code.gitea.io/gitea/models/db"
 
+// Strings for sorting result
+const (
+	// only used for repos
+	SearchOrderByAlphabetically        db.SearchOrderBy = "owner_name ASC, name ASC"
+	SearchOrderByAlphabeticallyReverse db.SearchOrderBy = "owner_name DESC, name DESC"
+	SearchOrderBySize                  db.SearchOrderBy = "size ASC"
+	SearchOrderBySizeReverse           db.SearchOrderBy = "size DESC"
+	SearchOrderByGitSize               db.SearchOrderBy = "git_size ASC"
+	SearchOrderByGitSizeReverse        db.SearchOrderBy = "git_size DESC"
+	SearchOrderByLFSSize               db.SearchOrderBy = "lfs_size ASC"
+	SearchOrderByLFSSizeReverse        db.SearchOrderBy = "lfs_size DESC"
+	// alias as also used elsewhere
+	SearchOrderByLeastUpdated  db.SearchOrderBy = db.SearchOrderByLeastUpdated
+	SearchOrderByRecentUpdated db.SearchOrderBy = db.SearchOrderByRecentUpdated
+	SearchOrderByOldest        db.SearchOrderBy = db.SearchOrderByOldest
+	SearchOrderByNewest        db.SearchOrderBy = db.SearchOrderByNewest
+	SearchOrderByID            db.SearchOrderBy = db.SearchOrderByID
+	SearchOrderByIDReverse     db.SearchOrderBy = db.SearchOrderByIDReverse
+	SearchOrderByStars         db.SearchOrderBy = db.SearchOrderByStars
+	SearchOrderByStarsReverse  db.SearchOrderBy = db.SearchOrderByStarsReverse
+	SearchOrderByForks         db.SearchOrderBy = db.SearchOrderByForks
+	SearchOrderByForksReverse  db.SearchOrderBy = db.SearchOrderByForksReverse
+)
+
 // SearchOrderByMap represents all possible search order
 var SearchOrderByMap = map[string]map[string]db.SearchOrderBy{
 	"asc": {
-		"alpha":   "owner_name ASC, name ASC",
-		"created": db.SearchOrderByOldest,
-		"updated": db.SearchOrderByLeastUpdated,
-		"size":    db.SearchOrderBySize,
-		"id":      db.SearchOrderByID,
+		"alpha":    SearchOrderByAlphabetically,
+		"created":  SearchOrderByOldest,
+		"updated":  SearchOrderByLeastUpdated,
+		"size":     SearchOrderBySize,
+		"git_size": SearchOrderByGitSize,
+		"lfs_size": SearchOrderByLFSSize,
+		"id":       SearchOrderByID,
+		"stars":    SearchOrderByStars,
+		"forks":    SearchOrderByForks,
 	},
 	"desc": {
-		"alpha":   "owner_name DESC, name DESC",
-		"created": db.SearchOrderByNewest,
-		"updated": db.SearchOrderByRecentUpdated,
-		"size":    db.SearchOrderBySizeReverse,
-		"id":      db.SearchOrderByIDReverse,
+		"alpha":    SearchOrderByAlphabeticallyReverse,
+		"created":  SearchOrderByNewest,
+		"updated":  SearchOrderByRecentUpdated,
+		"size":     SearchOrderBySizeReverse,
+		"git_size": SearchOrderByGitSizeReverse,
+		"lfs_size": SearchOrderByLFSSizeReverse,
+		"id":       SearchOrderByIDReverse,
+		"stars":    SearchOrderByStarsReverse,
+		"forks":    SearchOrderByForksReverse,
 	},
+}
+
+// SearchOrderByFlatMap is similar to SearchOrderByMap but use human language keywords
+// to decide between asc and desc
+var SearchOrderByFlatMap = map[string]db.SearchOrderBy{
+	"newest":                SearchOrderByMap["desc"]["created"],
+	"oldest":                SearchOrderByMap["asc"]["created"],
+	"leastupdate":           SearchOrderByMap["asc"]["updated"],
+	"reversealphabetically": SearchOrderByMap["desc"]["alpha"],
+	"alphabetically":        SearchOrderByMap["asc"]["alpha"],
+	"reversesize":           SearchOrderByMap["desc"]["size"],
+	"size":                  SearchOrderByMap["asc"]["size"],
+	"reversegitsize":        SearchOrderByMap["desc"]["git_size"],
+	"gitsize":               SearchOrderByMap["asc"]["git_size"],
+	"reverselfssize":        SearchOrderByMap["desc"]["lfs_size"],
+	"lfssize":               SearchOrderByMap["asc"]["lfs_size"],
+	"moststars":             SearchOrderByMap["desc"]["stars"],
+	"feweststars":           SearchOrderByMap["asc"]["stars"],
+	"mostforks":             SearchOrderByMap["desc"]["forks"],
+	"fewestforks":           SearchOrderByMap["asc"]["forks"],
 }

models/db/search.go
@@ -18,12 +18,6 @@ const (
 	SearchOrderByRecentUpdated         SearchOrderBy = "updated_unix DESC"
 	SearchOrderByOldest                SearchOrderBy = "created_unix ASC"
 	SearchOrderByNewest                SearchOrderBy = "created_unix DESC"
-	SearchOrderBySize                  SearchOrderBy = "size ASC"
-	SearchOrderBySizeReverse           SearchOrderBy = "size DESC"
-	SearchOrderByGitSize               SearchOrderBy = "git_size ASC"
-	SearchOrderByGitSizeReverse        SearchOrderBy = "git_size DESC"
-	SearchOrderByLFSSize               SearchOrderBy = "lfs_size ASC"
-	SearchOrderByLFSSizeReverse        SearchOrderBy = "lfs_size DESC"
 	SearchOrderByID                    SearchOrderBy = "id ASC"
 	SearchOrderByIDReverse             SearchOrderBy = "id DESC"
 	SearchOrderByStars                 SearchOrderBy = "num_stars ASC"

models/repo/repo_list.go
@@ -205,31 +205,6 @@ type SearchRepoOptions struct {
 	OnlyShowRelevant bool
 }
 
-// SearchOrderBy is used to sort the result
-type SearchOrderBy string
-
-func (s SearchOrderBy) String() string {
-	return string(s)
-}
-
-// Strings for sorting result
-const (
-	SearchOrderByAlphabetically        SearchOrderBy = "name ASC"
-	SearchOrderByAlphabeticallyReverse SearchOrderBy = "name DESC"
-	SearchOrderByLeastUpdated          SearchOrderBy = "updated_unix ASC"
-	SearchOrderByRecentUpdated         SearchOrderBy = "updated_unix DESC"
-	SearchOrderByOldest                SearchOrderBy = "created_unix ASC"
-	SearchOrderByNewest                SearchOrderBy = "created_unix DESC"
-	SearchOrderBySize                  SearchOrderBy = "size ASC"
-	SearchOrderBySizeReverse           SearchOrderBy = "size DESC"
-	SearchOrderByID                    SearchOrderBy = "id ASC"
-	SearchOrderByIDReverse             SearchOrderBy = "id DESC"
-	SearchOrderByStars                 SearchOrderBy = "num_stars ASC"
-	SearchOrderByStarsReverse          SearchOrderBy = "num_stars DESC"
-	SearchOrderByForks                 SearchOrderBy = "num_forks ASC"
-	SearchOrderByForksReverse          SearchOrderBy = "num_forks DESC"
-)
-
 // UserOwnedRepoCond returns user ownered repositories
 func UserOwnedRepoCond(userID int64) builder.Cond {
 	return builder.Eq{

routers/api/v1/repo/repo.go
@@ -107,7 +107,7 @@ func Search(ctx *context.APIContext) {
 	// - name: sort
 	//   in: query
 	//   description: sort repos by attribute. Supported values are
-	//                "alpha", "created", "updated", "size", and "id".
+	//                "alpha", "created", "updated", "size", "git_size", "lfs_size", "stars", "forks" and "id".
 	//                Default is "alpha"
 	//   type: string
 	// - name: order

routers/web/explore/repo.go
@@ -6,6 +6,7 @@ package explore
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"code.gitea.io/gitea/models/db"
 	repo_model "code.gitea.io/gitea/models/repo"
@@ -57,47 +58,18 @@ func RenderRepoSearch(ctx *context.Context, opts *RepoSearchOptions) {
 		orderBy db.SearchOrderBy
 	)
 
-	sortOrder := ctx.FormString("sort")
+	sortOrder := strings.ToLower(ctx.FormString("sort"))
 	if sortOrder == "" {
 		sortOrder = setting.UI.ExploreDefaultSort
 	}
-	ctx.Data["SortType"] = sortOrder
 
-	switch sortOrder {
-	case "newest":
-		orderBy = db.SearchOrderByNewest
-	case "oldest":
-		orderBy = db.SearchOrderByOldest
-	case "leastupdate":
-		orderBy = db.SearchOrderByLeastUpdated
-	case "reversealphabetically":
-		orderBy = db.SearchOrderByAlphabeticallyReverse
-	case "alphabetically":
-		orderBy = db.SearchOrderByAlphabetically
-	case "reversesize":
-		orderBy = db.SearchOrderBySizeReverse
-	case "size":
-		orderBy = db.SearchOrderBySize
-	case "reversegitsize":
-		orderBy = db.SearchOrderByGitSizeReverse
-	case "gitsize":
-		orderBy = db.SearchOrderByGitSize
-	case "reverselfssize":
-		orderBy = db.SearchOrderByLFSSizeReverse
-	case "lfssize":
-		orderBy = db.SearchOrderByLFSSize
-	case "moststars":
-		orderBy = db.SearchOrderByStarsReverse
-	case "feweststars":
-		orderBy = db.SearchOrderByStars
-	case "mostforks":
-		orderBy = db.SearchOrderByForksReverse
-	case "fewestforks":
-		orderBy = db.SearchOrderByForks
-	default:
-		ctx.Data["SortType"] = "recentupdate"
+	if order, ok := repo_model.SearchOrderByFlatMap[sortOrder]; ok {
+		orderBy = order
+	} else {
+		sortOrder = "recentupdate"
 		orderBy = db.SearchOrderByRecentUpdated
 	}
+	ctx.Data["SortType"] = sortOrder
 
 	keyword := ctx.FormTrim("q")
 

templates/swagger/v1_json.tmpl
@@ -3616,7 +3616,7 @@
           },
           {
             "type": "string",
-            "description": "sort repos by attribute. Supported values are \"alpha\", \"created\", \"updated\", \"size\", and \"id\". Default is \"alpha\"",
+            "description": "sort repos by attribute. Supported values are \"alpha\", \"created\", \"updated\", \"size\", \"git_size\", \"lfs_size\", \"stars\", \"forks\" and \"id\". Default is \"alpha\"",
             "name": "sort",
             "in": "query"
           },

yonasBSD/forgejo

commit df38c41c7a55035a8fe04a500d7fa0cfadc62ef1

fix: Add `recentupdated` as recognized sort option
- Add `recentupdated` to the `OrderByMap`.
- Add integration testing for organization and user repository sorting.
- Resolves #5612
- Regression from 12e23ee19916f4ca3f10916be8bb8666fd0d6f58 where the
`recentupdated` case was not added to the map, but was handled
seperately as a fallback. The regression came into affect when
5a0bc357992dad725b0163105f8b6464ec130a21 also relied on this map but
didn't handle the `recentupdated` case.

models/fixtures/repository.yml
@@ -91,6 +91,8 @@
   size: 0
   is_fsck_enabled: true
   close_issues_via_commit_in_any_branch: false
+  created_unix: 1700000001
+  updated_unix: 1700000001
 
 -
   id: 4
@@ -152,6 +154,8 @@
   size: 0
   is_fsck_enabled: true
   close_issues_via_commit_in_any_branch: false
+  created_unix: 1700000002
+  updated_unix: 1700000002
 
 -
   id: 6
@@ -182,6 +186,8 @@
   size: 0
   is_fsck_enabled: true
   close_issues_via_commit_in_any_branch: false
+  created_unix: 1710000001
+  updated_unix: 1710000001
 
 -
   id: 7
@@ -212,6 +218,8 @@
   size: 0
   is_fsck_enabled: true
   close_issues_via_commit_in_any_branch: false
+  created_unix: 1710000003
+  updated_unix: 1710000003
 
 -
   id: 8
@@ -242,6 +250,8 @@
   size: 0
   is_fsck_enabled: true
   close_issues_via_commit_in_any_branch: false
+  created_unix: 1710000002
+  updated_unix: 1710000002
 
 -
   id: 9
@@ -968,6 +978,8 @@
   size: 0
   is_fsck_enabled: true
   close_issues_via_commit_in_any_branch: false
+  created_unix: 1700000003
+  updated_unix: 1700000003
 
 -
   id: 33

models/repo/search.go
@@ -36,6 +36,7 @@ var OrderByMap = map[string]map[string]db.SearchOrderBy{
 var OrderByFlatMap = map[string]db.SearchOrderBy{
 	"newest":                OrderByMap["desc"]["created"],
 	"oldest":                OrderByMap["asc"]["created"],
+	"recentupdate":          OrderByMap["desc"]["updated"],
 	"leastupdate":           OrderByMap["asc"]["updated"],
 	"reversealphabetically": OrderByMap["desc"]["alpha"],
 	"alphabetically":        OrderByMap["asc"]["alpha"],

models/organization/org_test.go
@@ -299,8 +299,8 @@ func TestAccessibleReposEnv_RepoIDs(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, expectedRepoIDs, repoIDs)
 	}
-	testSuccess(2, []int64{3, 5, 32})
-	testSuccess(4, []int64{3, 32})
+	testSuccess(2, []int64{32, 5, 3})
+	testSuccess(4, []int64{32, 3})
 }
 
 func TestAccessibleReposEnv_Repos(t *testing.T) {
@@ -318,8 +318,8 @@ func TestAccessibleReposEnv_Repos(t *testing.T) {
 		}
 		assert.Equal(t, expectedRepos, repos)
 	}
-	testSuccess(2, []int64{3, 5, 32})
-	testSuccess(4, []int64{3, 32})
+	testSuccess(2, []int64{32, 5, 3})
+	testSuccess(4, []int64{32, 3})
 }
 
 func TestAccessibleReposEnv_MirrorRepos(t *testing.T) {

tests/integration/last_updated_time_test.go
@@ -16,11 +16,10 @@ func TestRepoLastUpdatedTime(t *testing.T) {
 		user := "user2"
 		session := loginUser(t, user)
 
-		req := NewRequest(t, "GET", path.Join("explore", "repos"))
+		req := NewRequest(t, "GET", "/explore/repos?q=repo1")
 		resp := session.MakeRequest(t, req, http.StatusOK)
 		doc := NewHTMLParser(t, resp.Body)
 		node := doc.doc.Find(".flex-item-body").First()
-
 		{
 			buf := ""
 			findTextNonNested(t, node, &buf)

tests/integration/org_test.go
@@ -28,6 +28,7 @@ func TestOrgRepos(t *testing.T) {
 		users = []string{"user1", "user2"}
 		cases = map[string][]string{
 			"alphabetically":        {"repo21", "repo3", "repo5"},
+			"recentupdate":          {"repo21", "repo5", "repo3"},
 			"reversealphabetically": {"repo5", "repo3", "repo21"},
 		}
 	)

tests/integration/user_test.go
@@ -819,3 +819,27 @@ func TestUserTOTPEnrolled(t *testing.T) {
 		assert.True(t, called)
 	})
 }
+
+func TestUserRepos(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	cases := map[string][]string{
+		"alphabetically":        {"repo6", "repo7", "repo8"},
+		"recentupdate":          {"repo7", "repo8", "repo6"},
+		"reversealphabetically": {"repo8", "repo7", "repo6"},
+	}
+
+	session := loginUser(t, "user10")
+	for sortBy, repos := range cases {
+		req := NewRequest(t, "GET", "/user10?sort="+sortBy)
+		resp := session.MakeRequest(t, req, http.StatusOK)
+
+		htmlDoc := NewHTMLParser(t, resp.Body)
+
+		sel := htmlDoc.doc.Find("a.name")
+		assert.Len(t, repos, len(sel.Nodes))
+		for i := 0; i < len(repos); i++ {
+			assert.EqualValues(t, repos[i], strings.TrimSpace(sel.Eq(i).Text()))
+		}
+	}
+}