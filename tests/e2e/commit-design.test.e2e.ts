af5b11458f68a90343724d5df9f2d549649cb5ae
iamyaash <iamyaash@outlook.com>
2025-12-19 13:23:53 +0530

    chore(test): playwright test for new PR commit design layout
    - test covers visibility & positions on both desktop & mobile viewports
    
    Signed-off-by: iamyaash <iamyaash@outlook.com>

---
+++ tests/e2e/commit-design.test.e2e.ts
@@ -0,0 +1,52 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// @watch start
+// templates/repo/diff/new_review.tmpl
+// web_src/js/features/repo-issue.js
+// @watch end
+
+import {expect} from '@playwright/test';
+import {test} from './utils_e2e.ts';
+import {screenshot} from './shared/screenshots.ts';
+
+test.use({user: 'user2'});
+
+test('PR Commits: mobile responsive layout checks', async ({page}) => {
+  const response = await page.goto('/user2/repo1/pulls/3/commits');
+  expect(response?.status()).toBe(200);
+  //mobile view
+  await page.setViewportSize({ width: 375, height: 667 });
+  
+  // mobile-specific grid positioning
+  await expect(page.locator('.commit-timeline .author').first()).toHaveCSS('grid-column-start', '1');
+  await expect(page.locator('.commit-timeline .date').first()).toHaveCSS('grid-column-start', '2');
+  await expect(page.locator('.commit-timeline .message').first()).toHaveCSS('grid-column-end', 'span 2');
+  await expect(page.locator('.commit-timeline .mobile-shabox').first()).toHaveCSS('grid-column-start', '1')
+  await expect(page.locator('.commit-timeline details').first()).toHaveCSS('grid-column-start', '2');
+
+  // mobile-specific visibility test
+  await expect(page.locator('.mobile-shabox').first()).toBeVisible();
+  await expect(page.locator('.commit-group h4').first()).toBeVisible();
+  await expect(page.locator('.shabox').first()).toBeHidden();
+  await expect(page.locator('.commit-buttons').first()).toBeHidden();
+
+  await expect(page.locator('.commit-group-commits').first()).not.toHaveCSS('overflow-x', 'scroll');
+  
+  await screenshot(page);
+});
+
+
+test('PR Commits: desktop responsive layout checks', async ({page}) => {
+  const response = await page.goto('/user2/repo1/pulls/3/commits');
+  expect(response?.status()).toBe(200);
+  // desktop view
+  await page.setViewportSize({ width: 1200, height: 800});
+
+  // desktop-specific visibility test
+  await expect(page.locator('.mobile-shabox').first()).toBeHidden();
+  await expect(page.locator('.shabox').first()).toBeVisible();
+  await expect(page.locator('.commit-buttons').first()).toBeVisible();
+
+  await screenshot(page);
+}); 
