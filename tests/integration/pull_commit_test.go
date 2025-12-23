76448261208e7c03927488a673648101b53bcfaa
iamyaash <iamyaash@outlook.com>
2025-12-23 17:40:37 +0530

    chore(test): updating correct selectors for integration tests
    Signed-off-by: iamyaash <iamyaash@outlook.com>

--- tests/integration/pull_commit_test.go
+++ tests/integration/pull_commit_test.go
@@ -53,7 +53,7 @@ func TestPullCommitLinks(t *testing.T) {
 
 	htmlDoc := NewHTMLParser(t, resp.Body)
 
-	commitSha := htmlDoc.Find(".commit-timeline .shabox .sha.label").First()
+	commitSha := htmlDoc.Find(".commit-timeline .shabox a").First()
 	commitShaHref, _ := commitSha.Attr("href")
 	assert.Equal(t, "/user2/repo1/pulls/3/commits/5f22f7d0d95d614d25a5b68592adb345a4b5c7fd", commitShaHref)
 
@@ -75,12 +75,12 @@ func TestPullCommitLinksSHA256(t *testing.T) {
 
 	htmlDoc := NewHTMLParser(t, resp.Body)
 
-	commitSha := htmlDoc.Find(".commit-list td.sha a.sha.label").First()
+	commitSha := htmlDoc.Find(".commit-timeline .shabox a").First()
 	commitShaHref, commitShaOk := commitSha.Attr("href")
 	assert.True(t, commitShaOk)
 	assert.Equal(t, "/user2/repo256/pulls/1/commits/004581b3bb63754502364664021404490ee747ce58e98d27c046f2e46f5f2f55", commitShaHref)
 
-	commitLink := htmlDoc.Find(".commit-list td.message a").First()
+	commitLink := htmlDoc.Find(".commit-timeline .message-wrapper a").First()
 	commitLinkHref, commitLinkOk := commitLink.Attr("href")
 	assert.True(t, commitLinkOk)
 	assert.Equal(t, "/user2/repo256/pulls/1/commits/004581b3bb63754502364664021404490ee747ce58e98d27c046f2e46f5f2f55", commitLinkHref)
