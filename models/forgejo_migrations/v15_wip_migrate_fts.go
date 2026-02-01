62f9c3b57b60ecd87ec1b4bb0349feda7c018150
Merlijn Wajer <merlijn@wizzup.org>
2026-02-01 16:41:28 +0100

    wip: fixup: add migration for new isCodeIndexerEnabled variable

---
+++ models/forgejo_migrations/v15_wip_migrate_fts.go
@@ -0,0 +1,22 @@
+// Copyright 2024 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package forgejo_migrations //nolint:revive
+
+import "xorm.io/xorm"
+
+func AddIsCodeIndexerEnabledToRepository(x *xorm.Engine) error {
+	type Repository struct {
+		ID                   int64 `xorm:"pk autoincr"`
+		IsCodeIndexerEnabled bool  `xorm:"NOT NULL DEFAULT true"`
+	}
+
+	return x.Sync(&Repository{})
+}
+
+func init() {
+	registerMigration(&Migration{
+		Description: "Add is_code_indexer_enabled to repository",
+		Upgrade:     AddIsCodeIndexerEnabledToRepository,
+	})
+}
